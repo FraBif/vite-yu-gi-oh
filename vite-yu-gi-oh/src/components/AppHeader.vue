2a1fa18c5cdb84a5f08937aa080f1fa1007a2ac4
FraBif <francescobif@hotmail.it>
2023-06-19 20:54:08 +0200

    populated header

--- vite-yu-gi-oh/src/components/AppHeader.vue
+++ vite-yu-gi-oh/src/components/AppHeader.vue
@@ -1,11 +1,35 @@
 <script>
+    export default {
+        name:'AppHeader',
+        data(){
+            return {
 
+            }
+        }
+    }
 </script>
 
 <template>
-
+    <div class="head-container">
+        <img src="../assets/img/logo.png" alt="yugi-logo" class="logo">
+        Yu-gi-Oh API 
+    </div>
 </template>
 
 <style>
+header{
+    width: 300px;
+}
 
+.logo{
+    display: inline-block;
+    width: 200px;
+}
+
+.head-container{
+    display: flex;
+    text-align: start;
+    align-items: center ;
+    font-size: 2rem;
+}
 </style>
